6cb4fbfd60a282037862bfc88512b8f34808f8fd
Gowynn Hunt <gowynnhunt@gmail.com>
2024-07-04 17:47:13 -0500

    Progress Update 2
    Finishing up Object Basics from the Odin Project.

---
+++ 04 - Array Cardio Day 1/script.js
@@ -0,0 +1,160 @@
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+
+// Some data we can work with
+
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+const people = [
+  "Bernhard, Sandra",
+  "Bethea, Erin",
+  "Becker, Carl",
+  "Bentsen, Lloyd",
+  "Beckett, Samuel",
+  "Blake, William",
+  "Berger, Ric",
+  "Beddoes, Mick",
+  "Beethoven, Ludwig",
+  "Belloc, Hilaire",
+  "Begin, Menachem",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Blair, Robert",
+  "Benenson, Peter",
+  "Benjamin, Walter",
+  "Berlin, Irving",
+  "Benn, Tony",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Berle, Milton",
+  "Berry, Halle",
+  "Biko, Steve",
+  "Beck, Glenn",
+  "Bergman, Ingmar",
+  "Black, Elk",
+  "Berio, Luciano",
+  "Berne, Eric",
+  "Berra, Yogi",
+  "Berry, Wendell",
+  "Bevan, Aneurin",
+  "Ben-Gurion, David",
+  "Bevel, Ken",
+  "Biden, Joseph",
+  "Bennington, Chester",
+  "Bierce, Ambrose",
+  "Billings, Josh",
+  "Birrell, Augustine",
+  "Blair, Tony",
+  "Beecher, Henry",
+  "Biondo, Frank",
+];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+const sixteenCentury = inventors.filter((person) => {
+  if (person.year >= 1500 && person.year < 1600) {
+    return true;
+  }
+});
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
+const firstLast = inventors.map((person) => {
+  return { first: person.first, last: person.last };
+});
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+const birthdate = inventors.sort((person1, person2) =>
+  person1.year > person2.year ? 1 : -1,
+);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+const totalYearsLived = inventors.reduce((total, person) => {
+  const lived = person.passed - person.year;
+  return total + lived;
+}, 0);
+
+// 5. Sort the inventors by years lived
+const yearslived = inventors.sort((person1, person2) => {
+  const netYears1 = person1.passed - person1.year;
+  const netYears2 = person2.passed - person2.year;
+
+  person1.total = netYears1;
+  person2.total = netYears2;
+
+  return netYears1 > netYears2 ? -1 : 1;
+});
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+const links = [...document.querySelectorAll(".mw-category a")];
+const de = links
+  .map((link) => link.textContent)
+  .filter((boulevard) => boulevard.includes("de"));
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const lastNames = people.sort((a, b) => {
+  const [aLast] = a.split(", ");
+  const [bLast] = b.split(", ");
+
+  return aLast > bLast ? 1 : -1;
+});
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+];
+
+// const sum = {};
+// for (let i = 0; i < data.length; i++) {
+//   const vehicle = data[i];
+
+//   // Create property if undefined
+//   if (!sum[vehicle]) {
+//     sum[vehicle] = 1;
+//     continue;
+//   }
+
+//   // Add 1 to property
+//   sum[vehicle] += 1;
+// }
+
+const sum = data.reduce((obj, vehicle) => {
+  if (obj[vehicle]) {
+    obj[vehicle]++;
+  } else {
+    obj[vehicle] = 1;
+  }
+  return obj;
+}, {});
+
+console.table(sum);
